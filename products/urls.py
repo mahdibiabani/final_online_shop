7693bbb56bda6a49d91528df2048b03c663ca3ce
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2024-03-25 13:31:38 +0330

    add product-list view

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ app_name = 'products'
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('category/', views.category_list_view, name='category_list'),
+    path('products/', views.product_list_view, name='product_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,8 +6,26 @@ from products.models import (Product, Category, Vendor, CartOrder, CartOrderItem
 
 
 def index(request):
-    products = Product.objects.all()
-    context ={
+    products = Product.objects.filter(product_status="published", featured=True)
+    context = {
         'products': products
     }
     return render(request, 'products/home.html', context)
+
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published")
+
+    context = {
+        "products": products
+    }
+    return render(request, 'products/product-list.html', context)
+
+
+def category_list_view(request):
+    categories = Category.objects.all()
+
+    context = {
+        "categories": categories
+    }
+    return render(request, 'products/category-list.html', context)
